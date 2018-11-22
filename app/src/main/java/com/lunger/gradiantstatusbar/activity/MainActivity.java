f7ef8991e2c5debad45b7289b83ad6e0cde354b6
Allen <836414757@qq.com>
2018-11-22 10:30:01 +0800

    Initial commit

---
+++ app/src/main/java/com/lunger/gradiantstatusbar/activity/MainActivity.java
@@ -0,0 +1,26 @@
+package com.lunger.gradiantstatusbar.activity;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.lunger.gradiantstatusbar.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public void solution1(View view) {
+        startActivity(new Intent(this, Solution1Activity.class));
+    }
+
+    public void solution2(View view) {
+        
+    }
+}
